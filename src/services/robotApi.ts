ffc3c554afae402d957a598a5d45963c5bdca8ae
ECM <101433020+cvonayres@users.noreply.github.com>
2025-09-02 05:26:03 -0700

    Update robotApi.ts

--- src/services/robotApi.ts
+++ src/services/robotApi.ts
@@ -59,6 +59,20 @@ export const robotApi = {
   moveLeft: () => makeRequest("/api/move/left", "POST"),
   moveRight: () => makeRequest("/api/move/right", "POST"),
   stop: () => makeRequest("/api/move/stop", "POST"),
+    speedUp: () => makeRequest("/api/move/speedup", "POST"),
+  speedDown: () => makeRequest("/api/move/speeddown", "POST"),
+  rotateTopLeft: () => makeRequest("/api/rotate/topleft", "POST"),
+  rotateTopRight: () => makeRequest("/api/rotate/topright", "POST"),
+  rotateBottomLeft: () => makeRequest("/api/rotate/bottomleft", "POST"),
+  rotateBottomRight: () => makeRequest("/api/rotate/bottomright", "POST"),
+  spinLeft: () => makeRequest("/api/rotate/topleft", "POST"),
+  spinRight: () => makeRequest("/api/rotate/topright", "POST"),
+  spinBackLeft: () => makeRequest("/api/rotate/bottomleft", "POST"),
+  spinBackRight: () => makeRequest("/api/rotate/bottomright", "POST"),
+  dance: (id: number) => makeRequest(`/api/move/dance/${id}`, "POST"),
+  playSong: (id: number) => makeRequest(`/api/song/${id}`, "POST"),
+  speak: (id: number) => makeRequest(`/api/say/${id}`, "POST"),
+  say: (id: number) => makeRequest(`/api/say/${id}`, "POST"),
 
   /**
    * Send an 8x8 LED matrix to the Sense HAT.
